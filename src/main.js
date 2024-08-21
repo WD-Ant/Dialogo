1f9e15dd87e45e17128e3a7d056e904a3b743498
Anthony Hoang <hoanganthony2207@gmail.com>
2024-08-21 17:31:11 -0400

    added preloader

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import '../styles/style.css';
 import '../styles/utils.css';
+import '../styles/components/preloader.css';
 
+import preloader from './utils/preloader';
 import sendMessage from './utils/sendMessage';
 
+preloader()
 sendMessage();
